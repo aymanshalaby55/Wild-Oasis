ec51e29fd0522757d39733883a287c6b2454e77d
Ayman Shalaby <aymanshalaby539@gmail.com>
2025-05-07 04:28:08 +0300

    first commit

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
+
+function Cabins() {
+  useEffect( function () {
+     getCabins().then((data)=> console.log(data));
+    // console.log(data);
+  },[]);
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>TEST</p>
+      <img src="" alt="" />
+    </Row>
+  );
+}
+
+export default Cabins;

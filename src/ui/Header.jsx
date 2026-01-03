a0354e1109e098e17b06f52d0f0d2c3522505132
ayman <aymanshalaby539@gmail.com>
2026-01-03 16:47:26 +0200

    feat: implement signup form and user avatar components, update package scripts

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
 const StyledHeader = styled.header`
+  display: flex;
+  justify-content: end;
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
 `;
 function Header() {
   return <StyledHeader>
-    <Logout></Logout>
+    <HeaderMenu></HeaderMenu>
   </StyledHeader>;
 }
 
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import UserAvatar from "../features/authentication/UserAvatar";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <UserAvatar />
+      </li>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout></Logout>
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
